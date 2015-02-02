5174f66163189deb7037fac68cf01ce914b7e6fa
sbt-hodakovskiy-da <SBT-Hodakovskiy-DA@mail.ca.sbrf.ru>
2015-02-02 11:53:52 +0300

    add sicontext generator classes

---
+++ mock-example/src/main/java/ru/sbt/bpm/mock/sigeneator/GenerateMockAppServlet.java
@@ -0,0 +1,95 @@
+package ru.sbt.bpm.mock.sigeneator;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+import javafx.util.Pair;
+import ru.sbt.bpm.mock.sigeneator.inentities.*;
+
+import java.io.FileReader;
+
+/**
+ * Created by sbt-hodakovskiy-da on 30.01.2015.
+ * <p/>
+ * Company: SBT - Saint-Petersburg
+ */
+// Singleton
+public class GenerateMockAppServlet {
+
+    private static GenerateMockAppServlet INSTANCE = null;
+
+    private String aFilePath;
+
+    private MockConfig aMockConfig;
+
+    private GenerateMockAppServlet() {
+        this.aFilePath = "";
+    }
+
+    private GenerateMockAppServlet(String aFilePath) {
+        this.aFilePath = aFilePath;
+    }
+
+    public void init() throws Exception{
+        if (this.aFilePath == null || this.aFilePath.equals(""))
+            throw new Exception();
+
+        FileReader fileReader = new FileReader(aFilePath);
+        XStream xStream = new XStream(new DomDriver());
+
+        // Mapping данных из xml в классы
+        xStream.processAnnotations(MockConfig.class);
+        xStream.processAnnotations(SystemsTag.class);
+        xStream.processAnnotations(SystemTag.class);
+        xStream.processAnnotations(IntegrationPoints.class);
+        xStream.processAnnotations(IntegrationPoint.class);
+        xStream.processAnnotations(LinkedTagSequence.class);
+        xStream.processAnnotations(LinkedTag.class);
+        xStream.processAnnotations(Dependencies.class);
+        xStream.processAnnotations(Dependency.class);
+        // parse
+        this.aMockConfig = (MockConfig) xStream.fromXML(fileReader);
+
+        // Дальше пишем куски для каждого блока SI context
+
+        System.out.println("End");
+    }
+
+
+    public static GenerateMockAppServlet getInstance() {
+        if (INSTANCE == null)
+            synchronized (GenerateMockAppServlet.class) {
+                if(INSTANCE == null)
+                    INSTANCE = new GenerateMockAppServlet();
+            }
+        return INSTANCE;
+    }
+
+    public static GenerateMockAppServlet getInstance(String aFilePath) {
+        if (INSTANCE == null)
+            synchronized (GenerateMockAppServlet.class) {
+                if(INSTANCE == null)
+                    INSTANCE = new GenerateMockAppServlet(aFilePath);
+            }
+        return INSTANCE;
+    }
+
+    public String getaFilePath() {
+        return aFilePath;
+    }
+
+    public void setaFilePath(String aFilePath) {
+        this.aFilePath = aFilePath;
+    }
+
+    public MockConfig getaMockConfig() {
+        return aMockConfig;
+    }
+
+    @Override
+    public String toString() {
+        return "GenerateMockAppServlet{" +
+                "aFilePath='" + aFilePath + '\'' +
+                '}';
+    }
+}
---
+++ mock-example/src/main/java/ru/sbt/bpm/mock/sigeneator/inentities/IntegrationPoint.java
@@ -0,0 +1,138 @@
+package ru.sbt.bpm.mock.sigeneator.inentities;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import ru.sbt.bpm.mock.sigeneator.Pair;
+
+import java.util.List;
+
+/**
+* Created by sbt-hodakovskiy-da on 30.01.2015.
+* <p/>
+* Company: SBT - Saint-Petersburg
+*/
+@XStreamAlias("integrationPoint")
+public class IntegrationPoint {
+
+    @XStreamAlias("name")
+    private String aIntegrationPointName;
+
+    @XStreamAlias("type")
+    private String aIntegrationPointType;
+
+    @XStreamAlias("linkedTagSequence")
+    private LinkedTagSequence aLinkedTagSequence;
+
+    @XStreamAlias("protocol")
+    private String aProtocol;
+
+    @XStreamAlias("incomeQueue")
+    private String aIncomeQueue;
+
+    @XStreamAlias("outcomeQueue")
+    private String aOutcomeQueue;
+
+    // Так как маппинг идет по полям xml, для удобства доступа и сравнения создаем Pair<INCOME, OUTCOME>
+    private Pair<String, String> aPairOfChannels;
+
+    @XStreamAlias("xsdFile")
+    private String aXsdFile;
+
+    @XStreamAlias("dependencies")
+    private Dependencies aDependencies;
+
+    public String getaIntegrationPointName() {
+        return aIntegrationPointName;
+    }
+
+    public void setaIntegrationPointName(String aIntegrationPointName) {
+        this.aIntegrationPointName = aIntegrationPointName;
+    }
+
+    public String getaIntegrationPointType() {
+        return aIntegrationPointType;
+    }
+
+    public void setaIntegrationPointType(String aIntegrationPointType) {
+        this.aIntegrationPointType = aIntegrationPointType;
+    }
+
+    public LinkedTagSequence getaLinkedTagSequence() {
+        return aLinkedTagSequence;
+    }
+
+    public void setaLinkedTagSequence(LinkedTagSequence aLinkedTagSequence) {
+        this.aLinkedTagSequence = aLinkedTagSequence;
+    }
+
+    public String getaProtocol() {
+        return aProtocol;
+    }
+
+    public void setaProtocol(String aProtocol) {
+        this.aProtocol = aProtocol;
+    }
+
+    public String getaIncomeQueue() {
+        return aIncomeQueue;
+    }
+
+    public void setaIncomeQueue(String aIncomeQueue) {
+        this.aIncomeQueue = aIncomeQueue;
+    }
+
+    public String getaOutcomeQueue() {
+        return aOutcomeQueue;
+    }
+
+    public void setaOutcomeQueue(String aOutcomeQueue) {
+        this.aOutcomeQueue = aOutcomeQueue;
+    }
+
+    public String getaXsdFile() {
+        return aXsdFile;
+    }
+
+    public void setaXsdFile(String aXsdFile) {
+        this.aXsdFile = aXsdFile;
+    }
+
+    public Dependencies getaDependencies() {
+        return aDependencies;
+    }
+
+    public void setaDependencies(Dependencies aDependencies) {
+        this.aDependencies = aDependencies;
+    }
+
+    public Pair<String, String> getaPairOfChannels() {
+        return aPairOfChannels;
+    }
+
+    public void setaPairOfChannels(Pair<String, String> aPairOfChannels) {
+        this.aPairOfChannels = aPairOfChannels;
+    }
+
+    public List<Dependency> getListOfDependencies() {
+        return aDependencies.getaDependency();
+    }
+
+    public List<LinkedTag> getListOfLinkedTags() {
+        return aLinkedTagSequence.getaListOfLinkedTags();
+    }
+
+    @Override
+    public String toString() {
+        return "IntegrationPoint{" +
+                "aIntegrationPointName='" + aIntegrationPointName + '\'' +
+                ", aIntegrationPointType='" + aIntegrationPointType + '\'' +
+                ", aLinkedTagSequence=" + aLinkedTagSequence +
+                ", aProtocol='" + aProtocol + '\'' +
+                ", aIncomeQueue='" + aIncomeQueue + '\'' +
+                ", aOutcomeQueue='" + aOutcomeQueue + '\'' +
+                ", aPairOfChannels=" + aPairOfChannels +
+                ", aXsdFile='" + aXsdFile + '\'' +
+                ", aDependencies=" + aDependencies +
+                '}';
+    }
+}
