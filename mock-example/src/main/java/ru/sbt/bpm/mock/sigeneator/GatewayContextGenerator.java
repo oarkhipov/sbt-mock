e64ad5da1a40900e8122ce1457543bd0677eacaf
sbt-hodakovskiy-da <SBT-Hodakovskiy-DA@mail.ca.sbrf.ru>
2015-02-03 11:39:03 +0300

    Рефакторинг. Создание строки для формировния gateway

--- mock-example/src/main/java/ru/sbt/bpm/mock/sigeneator/GatewayContextGenerator.java
+++ mock-example/src/main/java/ru/sbt/bpm/mock/sigeneator/GatewayContextGenerator.java
@@ -24,20 +24,23 @@ public class GatewayContextGenerator {
     // <Система, <Тип точки интеграции, <Входной канал, Выходной канал>>>
     private Map<String, Map<String, Pair<String, String>>> mapOfInOutChannelsByType;
     // Данный map служит для хранения уникальных значений каналов по их типу
-    // <Система, <Тип точки интеграции, <Входной канал, Выходной канал>>>
-    private Map<String, Map<String, Pair<String, String>>> mapOfInOutChannelsWithoutDuplicates;
+    // <Тип точки интеграции, <Входной канал, Выходной канал>>
+    private Map<String, Set<Pair<String, String>>> mapOfInOutChannelsWithoutDuplicates;
 
     public GatewayContextGenerator(List<SystemTag> systems) {
         this.aSystems = systems;
         this.mapOfInOutChannels = new HashMap<String, Map<String, Pair<String, String>>>();
         this.mapOfInOutChannelsByType = new HashMap<String, Map<String, Pair<String, String>>>();
-        this.mapOfInOutChannelsWithoutDuplicates = new HashMap<String, Map<String, Pair<String, String>>>();
+        this.mapOfInOutChannelsWithoutDuplicates = new HashMap<String, Set<Pair<String, String>>>();
     }
 
     // Раскладываем в мап все каналы
     public void putChannelsToMap() {
         if (aSystems == null || aSystems.isEmpty())
             return;
+        // Set<E> для удаления дубоикатов очередей
+        Set<Pair<String, String>> setOfInChannels = new HashSet<Pair<String, String>>();
+        Set<Pair<String, String>> setOfOutChannels = new HashSet<Pair<String, String>>();
 
         for (SystemTag system : aSystems) {
             Map<String, Pair<String, String>> mapOfIntPointChannels = new HashMap<String, Pair<String, String>>();
@@ -47,31 +50,44 @@ public class GatewayContextGenerator {
                 intPoint.setaPairOfChannels(new Pair<String, String>(intPoint.getaIncomeQueue(), intPoint.getaOutcomeQueue()));
                 mapOfIntPointChannels.put(intPoint.getaIntegrationPointName(), intPoint.getaPairOfChannels());
                 mapOfIntPointChannelsByType.put(intPoint.getaIntegrationPointType(), intPoint.getaPairOfChannels());
+
+                if (intPoint.getaIntegrationPointType().equals(MOCK_CONST))
+                    setOfInChannels.add(intPoint.getaPairOfChannels());
+                else
+                    setOfOutChannels.add(intPoint.getaPairOfChannels());
             }
 
             mapOfInOutChannels.put(system.getaSystemName(), mapOfIntPointChannels);
             mapOfInOutChannelsByType.put(system.getaSystemName(), mapOfIntPointChannelsByType);
+            mapOfInOutChannelsWithoutDuplicates.put(MOCK_CONST, setOfInChannels);
+            mapOfInOutChannelsWithoutDuplicates.put(DRIVER_CONST, setOfOutChannels);
         }
     }
 
     // Исключение дубликатов
+    // Функция больше не используется, так как для удаления дубликатов используется Set<E>
+    @Deprecated
     public void removeDuplicates() {
         Object[] keysSystem = mapOfInOutChannelsByType.keySet().toArray();
         int sizeKeysSystem = keysSystem.length;
 
         for (int i = 0; i < sizeKeysSystem - 1; i++)
             for (int j = i + 1; j < sizeKeysSystem; j++) {
-                HashMap<String, Pair<String, String>> hashMap = new HashMap<String, Pair<String, String>>();
+                HashMap<String, Pair<String, String>> hashMapMockChannels = new HashMap<String, Pair<String, String>>();
+                HashMap<String, Pair<String, String>> hashMapDriverChannels = new HashMap<String, Pair<String, String>>();
+                HashMap<String, Pair<String, String>> hashMapCommon = new HashMap<String, Pair<String, String>>();
                 boolean mockFlag = false;
                 boolean driverFlag = false;
                 if (mapOfInOutChannelsByType.get(keysSystem[i]).get(MOCK_CONST).equals(mapOfInOutChannelsByType.get(keysSystem[j]).get(MOCK_CONST))) {
                     mockFlag = true;
+                    hashMapCommon.put(MOCK_CONST, mapOfInOutChannelsByType.get(keysSystem[i]).get(MOCK_CONST));
                 } else {
 
                 }
 
                 if (mapOfInOutChannelsByType.get(keysSystem[i]).get(DRIVER_CONST).equals(mapOfInOutChannelsByType.get(keysSystem[j]).get(DRIVER_CONST))) {
                     driverFlag = true;
+                    hashMapCommon.put(DRIVER_CONST, mapOfInOutChannelsByType.get(keysSystem[i]).get(DRIVER_CONST));
                 } else {
 
                 }
@@ -84,13 +100,11 @@ public class GatewayContextGenerator {
         String sbOUT = null;
         StringBuilder sbCommon = new StringBuilder();
 
-        for (String  key : mapOfInOutChannelsByType.keySet())
-            for (String keyType : mapOfInOutChannelsByType.get(key).keySet())
-                if (keyType.equals(DRIVER_CONST)) {
-                    sbOUT = getGatewayDescription("outbound", "jmsout_" + key, mapOfInOutChannelsByType.get(key).get(keyType).getaFirst(), mapOfInOutChannelsByType.get(key).get(keyType).getaSecond());
-                } else if(keyType.equals(MOCK_CONST)) {
-                    sbIN = getGatewayDescription("inbound", "jmsin_" + key, mapOfInOutChannelsByType.get(key).get(keyType).getaFirst(), mapOfInOutChannelsByType.get(key).get(keyType).getaSecond());
-                }
+        for (Pair<String, String> mock : mapOfInOutChannelsWithoutDuplicates.get(MOCK_CONST))
+            sbCommon.append(getGatewayDescription("inbound", "jmsin_" + Math.random(), mock.getaFirst(), mock.getaSecond()));
+
+        for (Pair<String, String> driver : mapOfInOutChannelsWithoutDuplicates.get(MOCK_CONST))
+            sbCommon.append(getGatewayDescription("outbound", "jmsout_" + Math.random(), driver.getaFirst(), driver.getaSecond()));
 
         sbCommon.append(sbIN);
         sbCommon.append(sbOUT);
