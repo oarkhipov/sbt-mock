a073cecd531ae10caa3724978310c7e202c9b5a5
sbt-hodakovskiy-da <SBT-Hodakovskiy-DA@mail.ca.sbrf.ru>
2015-02-04 13:05:02 +0300

    Возмождность создавать любые bean, не зависимо от класса, с любым количеством разных property

--- mock-example/src/main/java/ru/sbt/bpm/mock/sigeneator/BeanGenerator.java
+++ mock-example/src/main/java/ru/sbt/bpm/mock/sigeneator/BeanGenerator.java
@@ -13,10 +13,10 @@ import java.util.Set;
  * Company: SBT - Saint-Petersburg
  */
 
-// Генератор JMS bean
+// Генератор bean
 public class BeanGenerator {
 
-    public static final String SPRING_FACTORY_CLASS = "org.springframework.jndi.JndiObjectFactoryBean";
+    private static final String SPRING_FACTORY_CLASS = "org.springframework.jndi.JndiObjectFactoryBean";
 
     // Генерация beans
     private String generationJMSBean(String beanId, String propertyValue) {
@@ -26,7 +26,10 @@ public class BeanGenerator {
 
     private List<SystemTag> aSystems;
 
+    @Deprecated
     private Set<Pair<String, String>> aSetOfQueue;
+
+
 
     public BeanGenerator(List<SystemTag> systems) {
         this.aSystems = systems;
@@ -34,6 +37,7 @@ public class BeanGenerator {
     }
 
     // получаем список очередей для передачи сообщений
+    @Deprecated
     public void putQueueIntoSet() {
         if (aSystems == null || aSystems.isEmpty())
             return;
@@ -44,6 +48,7 @@ public class BeanGenerator {
     }
 
     // Получаем стоку описания JSM bean
+    @Deprecated
     public String createBeans() {
         StringBuilder sb = new StringBuilder();
 
@@ -56,6 +61,38 @@ public class BeanGenerator {
             sb.append(generationJMSBean("replyQueue", queue.getaSecond()));
         }
 
+        return sb.toString();
+    }
+
+    /**
+     * Генериция bean любого типа и с любым количестовом property
+     * @param beanId - аттрибут id
+     * @param beanClass - аттрибут class
+     * @param properties - список property, где в Pair первое значение - name, второе значение - value
+     */
+    private void generateBean(String beanId, String beanClass, List<Pair<String, String>> properties) {
+        StringBuilder sb = new StringBuilder("<bean id=\"");
+        sb.append(beanId);
+        sb.append("\" class=\"");
+        sb.append(beanClass);
+        sb.append("\"");
+        if (properties == null || properties.isEmpty())
+            sb.append("/>\n");
+        else {
+            sb.append(">\n");
+                sb.append(generateProperty(properties));
+            sb.append("</bean>\n");
+        }
+    }
+
+    /**
+     * Создание property
+     * @param properties - список property, где в Pair первое значение - name, второе значение - value
+     */
+    private String generateProperty(List<Pair<String, String>> properties) {
+        StringBuilder sb = new StringBuilder();
+        for (Pair<String, String> property : properties)
+            sb.append("<property name=\"" + property.getaFirst() + "\" value=\"" + property.getaSecond() + "\"/>\n");
         return sb.toString();
     }
 }
