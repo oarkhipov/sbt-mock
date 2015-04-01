eb844da207fd70585e3821cf404c006386c39cda
SBT-Vostrikov-MI <SBT-Vostrikov-MI@mail.ca.sbrf.ru>
2015-04-01 17:35:02 +0300

    ручное разрешение мерджа - проверил подготовка к рефакторингу

--- mock-example/src/main/java/ru/sbt/bpm/mock/config/generator/util/importXSD.java
+++ mock-example/src/main/java/ru/sbt/bpm/mock/config/generator/util/importXSD.java
@@ -332,10 +332,9 @@ public class importXSD {
         importXSD instance = new importXSD();
         instance.initValidator(new File(instance.getWebInfPath() + "\\xsd"));
 
-//        instance.loadConfig("NCPConfig.xml");
-        //instance.loadConfig("BBMOConfig.xml");
-//        instance.loadConfig("BBMOConfig2.xml");
-        instance.loadConfig("KKMBConfig.xml");
+        //instance.loadConfig("NCPConfig.xml");
+        instance.loadConfig("BBMOConfig.xml");
+        //instance.loadConfig("KKMBConfig.xml");
     }
 
     public void loadConfig(String configFilename) throws Exception {
