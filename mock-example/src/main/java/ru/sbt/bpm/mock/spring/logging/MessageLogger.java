307b59016a18e0e61f1bf3f44a35c7558c43cef9
sbt-bochev-as <sbt-bochev-as@mail.ca.sbrf.ru>
2015-05-27 10:30:02 +0300

    Настройка логирования

--- mock-example/src/main/java/ru/sbt/bpm/mock/spring/logging/MessageLogger.java
+++ mock-example/src/main/java/ru/sbt/bpm/mock/spring/logging/MessageLogger.java
@@ -1,16 +1,12 @@
 package ru.sbt.bpm.mock.spring.logging;
 
-import org.springframework.stereotype.Service;
 
 /**
  * Created by sbt-bochev-as on 21.04.2015.
  * <p/>
  * Company: SBT - Moscow
  */
-@Service
-public class MessageLogger {
+public interface MessageLogger {
 
-    public void logMessage(Object payload, String queueName) {
-
-    }
+    void logMessage(Object payload, String queueName);
 }
