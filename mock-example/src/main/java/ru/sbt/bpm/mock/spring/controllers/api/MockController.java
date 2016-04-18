f6464b6f981fc58e5ad52c051c4a989ec0950c0e
sbt-bochev-as <sbt-bochev-as@mail.ca.sbrf.ru>
2016-04-18 10:48:50 +0300

    fixed test validation condition

--- mock-example/src/main/java/ru/sbt/bpm/mock/spring/controllers/api/MockController.java
+++ mock-example/src/main/java/ru/sbt/bpm/mock/spring/controllers/api/MockController.java
@@ -197,7 +197,7 @@ public class MockController {
         try {
             ru.sbt.bpm.mock.config.entities.System system = configContainer.getSystemByName(systemName);
             IntegrationPoint integrationPoint = system.getIntegrationPoints().getIntegrationPointByName(integrationPointName);
-            if (test == null || test.length() >0 || messageValidationService.assertXpath(test, system, integrationPoint, MessageType.RQ)) {
+            if (test == null || test.length() == 0 || messageValidationService.assertXpath(test, system, integrationPoint, MessageType.RQ)) {
                 compiledXml = groovyService.execute(test, xml, script);
                 if (messageValidationService.assertXpath(compiledXml, system, integrationPoint, MessageType.RS)) {
                     final List<String> validationErrors = messageValidationService.validate(compiledXml, systemName);
