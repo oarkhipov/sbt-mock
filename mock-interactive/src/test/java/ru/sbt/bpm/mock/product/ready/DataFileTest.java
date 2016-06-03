8ad40d86ddb4337c1114141d9dbf11b8a4401d50
sbt-hodakovskiy-da <SBT-Hodakovskiy-DA@mail.ca.sbrf.ru>
2016-06-03 16:40:06 +0300

    added getting test, message and groovy script

--- mock-interactive/src/test/java/ru/sbt/bpm/mock/product/ready/DataFileTest.java
+++ mock-interactive/src/test/java/ru/sbt/bpm/mock/product/ready/DataFileTest.java
@@ -1,10 +1,18 @@
 package ru.sbt.bpm.mock.product.ready;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.testng.annotations.Test;
+import ru.sbt.bpm.mock.config.MockConfigContainer;
+import ru.sbt.bpm.mock.config.entities.IntegrationPoint;
+import ru.sbt.bpm.mock.spring.service.DataFileService;
+import ru.sbt.bpm.mock.spring.service.GroovyService;
+import ru.sbt.bpm.mock.spring.service.XmlGeneratorService;
+
+import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
 
@@ -17,10 +25,64 @@ import static org.testng.Assert.assertTrue;
 @WebAppConfiguration("mock-interactive/src/main/webapp")
 public class DataFileTest extends AbstractTestNGSpringContextTests {
 
+	@Autowired
+	MockConfigContainer container;
+
+	@Autowired
+	DataFileService dataFileService;
+
+	@Autowired
+	TestMessageValidationService messageValidationService;
+
+	@Autowired
+	XmlGeneratorService xmlGeneratorService;
+
+	@Autowired
+	GroovyService groovyService;
 
 	@Test
 	public void tesDataFiles() {
 		boolean assertSuccess = true;
+
+		for (ru.sbt.bpm.mock.config.entities.System system : container.getConfig().getSystems().getSystems()) {
+			try {
+				log.info("===============================================================================================");
+				log.info("");
+				log.info(String.format("            DRIVER                      "));
+				log.info("");
+				log.info("===============================================================================================");
+				for (IntegrationPoint point : system.getDriverIntegrationPoints()) {
+						log.info("Integration point: " + point.getName());
+						String currentMessage = dataFileService.getCurrentMessage(system.getSystemName(), point.getName());
+						log.info("Current message: " + currentMessage);
+						String currentScript = dataFileService.getCurrentScript(system.getSystemName(), point.getName());
+						log.info("Current script: " + currentScript);
+
+	//					groovyService.execute();
+				}
+
+
+				log.info("===============================================================================================");
+				log.info("");
+				log.info(String.format("            MOCK                      "));
+				log.info("");
+				log.info("===============================================================================================");
+				for (IntegrationPoint point : system.getMockIntegrationPoints()) {
+					log.info("Integration point: " + point.getName());
+					String currentMessage = dataFileService.getCurrentMessage(system.getSystemName(), point.getName());
+					log.info("Current message: " + currentMessage);
+					String currentScript = dataFileService.getCurrentScript(system.getSystemName(), point.getName());
+					log.info("Current script: " + currentScript);
+					String currentTest = dataFileService.getCurrentTest(system.getSystemName(), point.getName());
+					log.info("Current test: " + currentTest);
+
+
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
 
 
 
