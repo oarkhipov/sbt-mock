101340266f34040f082c62f0ad3e28de63b35098
sbt-hodakovskiy-da <SBT-Hodakovskiy-DA@mail.ca.sbrf.ru>
2016-06-15 10:26:50 +0300

    add method getXsdResources

--- mock-interactive/src/main/java/ru/sbt/bpm/mock/spring/service/XsdAnalysisService.java
+++ mock-interactive/src/main/java/ru/sbt/bpm/mock/spring/service/XsdAnalysisService.java
@@ -3,8 +3,13 @@ package ru.sbt.bpm.mock.spring.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import ru.sbt.bpm.mock.config.MockConfigContainer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by sbt-hodakovskiy-da on 14.06.2016.
@@ -22,4 +27,15 @@ public class XsdAnalysisService {
 
 	@Autowired
 	DataFileService dataFileService;
+
+	public Map<String, Resource> getXsdResources () {
+		Map<String, Resource> map = new HashMap<String, Resource>();
+		for (ru.sbt.bpm.mock.config.entities.System system : configContainer.getConfig().getSystems().getSystems())
+			try {
+				System.out.println(system.getSystemName() + " xsd: " + dataFileService.getSystemXsdDirectoryResource(system.getSystemName()).getFile());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		return new HashMap<String, Resource>();
+	}
 }
