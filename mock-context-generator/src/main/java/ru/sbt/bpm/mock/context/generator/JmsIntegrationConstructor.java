803c50e96e1013b7954876c0c648240ff8dc6c38
sbt-hodakovskiy-da <SBT-Hodakovskiy-DA@mail.ca.sbrf.ru>
2016-07-07 15:13:53 +0300

    add methods and test for InboundGateway

--- mock-context-generator/src/main/java/ru/sbt/bpm/mock/context/generator/JmsIntegrationConstructor.java
+++ mock-context-generator/src/main/java/ru/sbt/bpm/mock/context/generator/JmsIntegrationConstructor.java
@@ -6,6 +6,58 @@ package ru.sbt.bpm.mock.context.generator;
 
 public class JmsIntegrationConstructor implements IContextGeneratable {
 
+	/**
+	 * <jms:inbound-gateway id="" request-destination="" request-channel="" reply-channel="" default-reply-destination="" connection-factory=""/>
+	 * @param beans
+	 * @param id
+	 * @param requestDestination
+	 * @param requestChannel
+	 * @param replyChannel
+	 * @param defaultReplyDestination
+	 * @param connectionFactory
+	 * @return
+	 */
+	public generated.springframework.beans.Beans createInboundGateway(generated.springframework.beans.Beans beans, String id, String requestDestination, String requestChannel, String replyChannel, String defaultReplyDestination, String connectionFactory) {
+		return createInboundGatewayInternal(beans, id, requestDestination, requestChannel, replyChannel, defaultReplyDestination, connectionFactory);
+	}
+
+	/**
+	 * <jms:inbound-gateway id="" request-destination="" request-channel="" reply-channel="" default-reply-destination="" connection-factory=""/>
+	 * @param beans
+	 * @param id
+	 * @param requestDestination
+	 * @param requestChannel
+	 * @param replyChannel
+	 * @param defaultReplyDestination
+	 * @param connectionFactory
+	 * @return
+	 */
+	private generated.springframework.beans.Beans createInboundGatewayInternal(generated.springframework.beans.Beans beans, String id, String requestDestination, String requestChannel, String replyChannel, String defaultReplyDestination, String connectionFactory) {
+		beans.getImportOrAliasOrBean().add(createInboundGateway(id, requestDestination, requestChannel, replyChannel, defaultReplyDestination, connectionFactory));
+		return beans;
+	}
+
+	/**
+	 * <jms:inbound-gateway id="" request-destination="" request-channel="" reply-channel="" default-reply-destination="" connection-factory=""/>
+	 * @param id
+	 * @param requestDestination
+	 * @param requestChannel
+	 * @param replyChannel
+	 * @param defaultReplyDestination
+	 * @param connectionFactory
+	 * @return
+	 */
+	private generated.springframework.integration.jms.InboundGateway createInboundGateway(String id, String requestDestination, String requestChannel, String replyChannel, String defaultReplyDestination, String connectionFactory) {
+		generated.springframework.integration.jms.InboundGateway inboundGateway = jmsFactory.createInboundGateway();
+		inboundGateway.setId(id);
+		inboundGateway.setRequestDestination(requestDestination);
+		inboundGateway.setRequestChannel(requestChannel);
+		inboundGateway.setReplyChannel(replyChannel);
+		inboundGateway.setDefaultReplyDestination(defaultReplyDestination);
+		inboundGateway.setConnectionFactory(connectionFactory);
+		return inboundGateway;
+	}
+
 	public generated.springframework.integration.jms.ObjectFactory getIntegrationFactory() {
 		return jmsFactory;
 	}
--- mock-context-generator/src/test/java/ru/sbt/bpm/mock/context/generator/JmsIntegrationConstructorTest.java
+++ mock-context-generator/src/test/java/ru/sbt/bpm/mock/context/generator/JmsIntegrationConstructorTest.java
@@ -1,5 +1,10 @@
 package ru.sbt.bpm.mock.context.generator;
 
+import generated.springframework.beans.Beans;
+import org.testng.annotations.Test;
+
+import javax.xml.bind.JAXBException;
+
 /**
  * Created by sbt-hodakovskiy-da on 05.07.2016.
  */
@@ -8,4 +13,27 @@ public class JmsIntegrationConstructorTest extends AbstractConfigGenerator {
 	BeansConstructor beansConstructor = new BeansConstructor();
 
 	JmsIntegrationConstructor jmsIntegrationConstructor = new JmsIntegrationConstructor();
+
+	@Test
+	public void testCreateInboundGateway() throws JAXBException {
+		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><beans "
+		                  + "xsi:schemaLocation=\"http://www.springframework.org/schema/beans        http://www"
+		                  + ".springframework.org/schema/beans/spring-beans.xsd\n"
+		                  + "        http://www.springframework.org/schema/integration    http://www.springframework"
+		                  + ".org/schema/integration/spring-integration.xsd\n"
+		                  + "        http://www.springframework.org/schema/integration http://www.springframework"
+		                  + ".org/schema/integration/spring-integration.xsd\n"
+		                  + "        http://www.springframework.org/schema/integration/jms http://www.springframework"
+		                  + ".org/schema/integration/jms/spring-integration-jms.xsd\" xmlns=\"http://www"
+		                  + ".springframework.org/schema/beans\" xmlns:jms=\"http://www.springframework"
+		                  + ".org/schema/integration/jms\" xmlns:int=\"http://www.springframework"
+		                  + ".org/schema/integration\" xmlns:xsi=\"http://www.w3"
+		                  + ".org/2001/XMLSchema-instance\"><jms:inbound-gateway request-destination=\"inboundMock\" "
+		                  + "default-reply-destination=\"defaultReplyChannel\" request-channel=\"inboubdRequest\" "
+		                  + "reply-channel=\"replyChannel\" id=\"idName\" "
+		                  + "connection-factory=\"jndiConnectionFactory\"/></beans>";
+		Beans beans = beansConstructor.createBeans();
+		beans = jmsIntegrationConstructor.createInboundGateway(beans, "idName", "inboundMock", "inboubdRequest", "replyChannel", "defaultReplyChannel", "jndiConnectionFactory");
+		assert compareResults(expected, beans, beansConstructor.getBeanFactory().getClass(), jmsIntegrationConstructor.getIntegrationFactory().getClass()) == 0;
+	}
 }
