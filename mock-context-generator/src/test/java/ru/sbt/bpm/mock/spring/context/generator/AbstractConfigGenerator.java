277bb5297933ee0c0c7f3c90c8003930008ed3b4
sbt-hodakovskiy-da <SBT-Hodakovskiy-DA@mail.ca.sbrf.ru>
2016-07-15 11:52:16 +0300

    fixed test assertion

--- mock-context-generator/src/test/java/ru/sbt/bpm/mock/spring/context/generator/AbstractConfigGenerator.java
+++ mock-context-generator/src/test/java/ru/sbt/bpm/mock/spring/context/generator/AbstractConfigGenerator.java
@@ -2,14 +2,20 @@ package ru.sbt.bpm.mock.spring.context.generator;
 
 import generated.springframework.beans.Beans;
 import lombok.extern.slf4j.Slf4j;
+import org.custommonkey.xmlunit.Diff;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.xml.sax.SAXException;
 import ru.sbt.bpm.mock.spring.context.generator.service.SpringContextGeneratorService;
 
 import javax.xml.bind.JAXBException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /**
  * Created by sbt-hodakovskiy-da on 06.07.2016.
@@ -33,7 +39,33 @@ public abstract class AbstractConfigGenerator extends AbstractTestNGSpringContex
 	protected void compareResults (String expected, Beans beans, String classAndMethodName) throws JAXBException {
 		String actual = generator.toXml(beans);
 		printActual(actual, classAndMethodName);
-		assertEquals(actual.replaceAll("\\s", ""), expected.replaceAll("\\s", ""));
+		String actualSting = compactXml(actual);
+		String expectedString = compactXml(expected);
+		try {
+			Diff diff = new Diff(actualSting, expectedString);
+			assertTrue(diff.identical(), diff.toString());
+		} catch (SAXException e ) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String compactXml(String xml) {
+		BufferedReader bufferedReader = new BufferedReader(new StringReader(xml));
+		String line;
+		StringWriter   stringWriter = new StringWriter();
+
+		try {
+			while ((line = bufferedReader.readLine()) != null) {
+				stringWriter.append(line.trim());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		xml = stringWriter.toString();
+		log.debug("Parsed xml: " + xml);
+		return xml;
 	}
 
 }
