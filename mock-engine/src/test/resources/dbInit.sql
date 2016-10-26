66b4e4315bd7c1949b5e96c209722f3a0901a329
sbt-bochev-as <sbt-bochev-as@mail.ca.sbrf.ru>
2016-10-26 15:34:29 +0300

    added MockChain table to db

--- mock-engine/src/test/resources/dbInit.sql
+++ mock-engine/src/test/resources/dbInit.sql
@@ -14,4 +14,19 @@ CREATE TABLE IF NOT EXISTS LOGS
 CREATE INDEX IF NOT EXISTS LOGS_MESS_PREVIEW_INDEX ON LOGS (MESSAGEPREVIEW);
 CREATE INDEX IF NOT EXISTS LOGS_TS_INDEX ON LOGS (TS);
 -- migrate 3.x.x to 3.6.2
-ALTER TABLE LOGS ADD TID VARCHAR(50) AFTER TS
+ALTER TABLE LOGS ADD TID VARCHAR(50) AFTER TS;
+
+-- add mock chain mechanism
+CREATE TABLE IF NOT EXISTS PUBLIC.chains
+(
+  id INT AUTO_INCREMENT NOT NULL,
+  ts TIMESTAMP DEFAULT NOW(),
+  triggerTime DATE NOT NULL,
+  system VARCHAR(100) NOT NULL,
+  integrationPoint VARCHAR(100) NOT NULL,
+  messageTemplateId UUID,
+  message CLOB NOT NULL,
+  CONSTRAINT chains_id_ts_pk PRIMARY KEY (id, ts)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "chains_id_uindex" ON PUBLIC.chains (id);
+CREATE INDEX IF NOT EXISTS "chains_triggerTime_system_integrationPoint_messageTemplateId_index" ON PUBLIC.chains (triggerTime DESC, system, integrationPoint, messageTemplateId);
